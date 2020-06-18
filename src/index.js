5d960564dd5e329bcff2023d34d57df1fcc8eb9a
sujay456 <63901058+sujay456@users.noreply.github.com>
2020-06-18 19:51:48 +0530

    conditions

--- src/index.js
+++ src/index.js
@@ -6,14 +6,42 @@ import ReactDOM from 'react-dom';
 
 class App extends React.Component{
     
-    // render method
+    // This is not react specific but js script
+    constructor(props)
+    {
+        super(props);
+        this.state={
+            // when we want to make some key but dont want to assign yet but eventually will fill it up ,then we use null
+            lat:null,
+            errorMssg:''
+        };
+        
+         window.navigator.geolocation.getCurrentPosition(
+            (pos)=>{ 
+                // To update our state we use setState 
+                this.setState({ lat:pos.coords.latitude })
+             },
+            (err)=>{ 
+                console.log(err);
+                this.setState({ errorMssg: err.message }); 
+            }
+        )
+    }
+    // render method (Which is required by react )
     render()
     {
-        window.navigator.geolocation.getCurrentPosition(
-            (pos)=>{  console.log(pos); },
-            (err)=>{ console.log(err); }
-        )
-        return <div>Lattitude:</div>
+        
+        if(this.state.errorMssg && !this.state.lat)
+        {
+            return <div>Error:{this.state.errorMssg} </div>
+        }
+
+        if(!this.state.errorMssg && this.state.lat )
+        {
+            return <div>latitude:{this.state.lat} </div>
+        }
+
+        return <div>Loading!!</div>
     }
 
 };
